f82f766a4c3370f907ba6fd26ad061b97011b37a
Komal-99 <komalgupta991000@gmail.com>
2023-06-28 19:49:25 +0530

    footer removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
